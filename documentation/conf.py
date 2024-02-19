0524341bc602dfba6e032240ed94e1ccc4cb17a6
Aline Baudry <aline.baudry@sorbonne-universite.fr>
2024-02-20 00:25:41 +0100

    add sphinx lesson

--- documentation/conf.py
+++ documentation/conf.py
@@ -30,7 +30,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['myst_parser', 'sphinx_rtd_theme']
+extensions = ['myst_parser', 'sphinx_rtd_theme', "sphinx_lesson"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

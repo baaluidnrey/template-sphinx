17d8927245f61108c9a60232b17b1b96f192fde5
alberge <alberge@isir.upmc.fr>
2024-02-19 16:51:14 +0100

    update color

--- documentation/conf.py
+++ documentation/conf.py
@@ -52,7 +52,8 @@ html_theme = 'classic'
 html_logo = 'theme/logoIsir.png'
 html_theme_options = {
     "relbarbgcolor": "#012235",
-    "sidebarbgcolor": "#0090CD"
+    "sidebarbgcolor": "white",
+    "headbgcolor": "#0090CD"
 }
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

9ce9963f7011c12c6ce10d363e096301e488bb16
Aline Baudry <aline.baudry@sorbonne-universite.fr>
2024-02-19 23:36:20 +0100

    mermaid

--- documentation/conf.py
+++ documentation/conf.py
@@ -30,7 +30,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['myst_parser', 'sphinx_rtd_theme', 'sphinxmermaid']
+extensions = ['myst_parser', 'sphinx_rtd_theme', 'sphinx_mermaid']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
